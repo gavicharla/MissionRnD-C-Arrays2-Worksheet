38fd71fafebb3d87d63e3e79dee068a748711ec5
gavicharla <gavicharla@missionrnd.com>
2016-01-11 17:51:24 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,13 +13,46 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
-
+#include <stdlib.h>
+#include<stdio.h>
 struct transaction {
 	int amount;
 	char date[11];
 	char description[20];
 };
+int date_to_num(char * s)
+{
+	int num = 0, i;
+	//char * s = (char*)malloc(sizeof(char) * 20);
+	for (i = 6; i <= 9; i++)
+	{
+		num = num * 10 + s[i] - '0';
+	}
+	num = num * 10 + s[3] - '0';
+	num = num * 10 + s[4] - '0';
 
+	num = num * 10 + s[0] - '0';
+	num = num * 10 + s[1] - '0';
+	return num;
+}
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+	if (Arr == NULL)
+		return -1;
+
+	//converting dates to number
+	int key = 0, i;
+	key = date_to_num(date);
+	int *date_num = (int *)malloc(sizeof(int)*len);
+	int count = 0;
+	for (i = 0; i < len; i++)
+	{
+		date_num[i] = date_to_num(Arr[i].date);
+		if (date_num[i]>key)
+		{
+			count++;
+			printf("%d %d\n", key, date_num[i]);
+		}
+	}
+	return count;
+
 }
