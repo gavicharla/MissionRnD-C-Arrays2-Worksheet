124f8e05cb49fe3bfeded3486997f67a360e20d0
gavicharla <gavicharla@missionrnd.com>
2016-01-13 13:09:58 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -37,15 +37,50 @@ int date_to_num2(char * s)
 	return num;
 }
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	if (A==NULL || B==NULL)
-	return NULL;
+	if (A == NULL || B == NULL)
+		return NULL;
 	struct transaction * result = (struct transaction *)malloc(sizeof(struct transaction)* (ALen + BLen));
-	int key1, key2;
-	for (int i = ALen, j = BLen; i >= 0 && j >= 0;i--)
+	// trading space for better time complexity 
+	int * first = (int *)malloc(sizeof(int)*ALen);
+	int * second = (int *)malloc(sizeof(int)*ALen);
+	for (int i = 0; i < ALen; i++)
+		first[i] = date_to_num2(A[i].date);
+
+	for (int i = 0; i < BLen; i++)
+		second[i] = date_to_num2(B[i].date);
+
+	int i, j;
+	// just like sorting arrays of sizing m and n ... takes time of O(m+n)
+	//this is better compared to usual string comparisions coz ... it takes O(n) to compare and then n to iterate ... O(m^2) for loops ... so it takes O(n*m^2) ... O(n^3) \
+		// so this is far better in complexity 
+	for (i = ALen - 1, j = BLen - 1; i >= 0 && j >= 0;)
 	{
-		
+		if (first[i]>second[j])
+		{
+			result[i + j + 1] = A[i];
+			i--;
+		}
+		else if (first[i] < second[j])
+		{
+			result[i + j + 1] = B[j];
+			j--;
+		}
+		else
+		{
+			result[i + j + 1] = B[j];
+			j--;
+			result[i + j + 1] = A[i];
+			i--;
+		}
 	}
-	
-	
-	return NULL;
+	int temp = j;
+
+	if (i >= 0)
+		for (j = 0; j <= i; j++)
+			result[j] = A[j];
+
+	if (temp >= 0)
+		for (i = 0; i <= temp; i++)
+			result[i] = B[i];
+	return result;
 }
